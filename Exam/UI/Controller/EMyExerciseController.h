be9b92c1fcb594752f7873ba4ec1759709d3a16b
yongqing3 <yongqing3@staff.sina.com.cn>
2017-05-05 14:33:52 +0800

    我的题库init

---
+++ Exam/UI/Controller/EMyExerciseController.h
@@ -0,0 +1,16 @@
+//
+//  EMyExerciseController.h
+//  Exam
+//
+//  Created by yongqingguo on 2017/5/5.
+//  Copyright © 2017年 shadeless. All rights reserved.
+//
+
+#import "EBaseController.h"
+
+/**
+ 我的题库
+ */
+@interface EMyExerciseController : EBaseController
+
+@end
---
+++ Exam/UI/Controller/EMyExerciseController.m
@@ -0,0 +1,50 @@
+//
+//  EMyExerciseController.m
+//  Exam
+//
+//  Created by yongqingguo on 2017/5/5.
+//  Copyright © 2017年 shadeless. All rights reserved.
+//
+
+#import "EMyExerciseController.h"
+
+@interface EMyExerciseController ()<UITableViewDelegate,UITableViewDataSource>
+{
+    UITableView *_tableView;
+}
+
+@end
+
+@implementation EMyExerciseController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.title = @"我的题库";
+    [self initTable];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)initTable {
+    _tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, kFrameWidth, kFrameHeight) style:UITableViewStyleGrouped];
+    _tableView.delegate = self;
+    _tableView.dataSource = self;
+    _tableView.showsVerticalScrollIndicator = NO;
+    [self.view addSubview:_tableView];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
